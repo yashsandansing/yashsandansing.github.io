3ca70f0cb3e0a0c374815594d7d8b1b1955eb121
Yash Sandansing <ysandansing@gmail.com>
2025-09-21 22:06:23 -0400

    init commit

---
+++ _next/static/chunks/48e8085a58da2b8f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/755dc5abe3f90192.js",
+  "static/chunks/turbopack-3275d9e2f88271de.js"
+])
---
+++ _next/static/chunks/c0a16bf2f990e19a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/755dc5abe3f90192.js",
+  "static/chunks/turbopack-bfd449d70090f518.js"
+])
