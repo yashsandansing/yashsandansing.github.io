6905ec304512d3eb8c6a0319888283f9e165ff6c
Yash Sandansing <ysandansing@gmail.com>
2025-09-21 22:51:19 -0400

    prefix added to profile image;

--- _next/static/chunks/79138a0fae1349a1.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/6f4fa4c1cd2d40f2.js",
-  "static/chunks/turbopack-1145cd58b4dc9d63.js"
-])
--- _next/static/chunks/7953247e640b39ea.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/e60ef129113f6e24.js",
-  "static/chunks/6f4fa4c1cd2d40f2.js",
-  "static/chunks/turbopack-43095cbe761bacbd.js"
-])
