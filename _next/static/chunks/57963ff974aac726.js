687f61b5bfcbe275d2d6dc4991c43704d9599de6
Yash Sandansing <ysandansing@gmail.com>
2026-04-21 01:33:31 -0400

    v2 major file changes

---
+++ _next/static/chunks/57963ff974aac726.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/6b5b6405f3be80ba.js",
+  "static/chunks/turbopack-4324af941509aeb6.js"
+])
---
+++ _next/static/chunks/d1372c3e6c6d16c7.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/6b5b6405f3be80ba.js",
+  "static/chunks/turbopack-483dd45d683259d8.js"
+])
